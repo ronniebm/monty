d7c0a5ac7a58342aa1e2ae5a1f0f9ef33ff3c2d5
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-12 12:17:18 -0500

    create prototype of main funtions

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * main - main funtion
+ * @argc: number of arguments
+ * @av: argumens passed to funtion in not interactive mode
+ * Return: Always a inter number
+ */
+
+int main(int argc, char **av, char **env)
+{
+
+}
