c8de39d9d68e9857548ca7d514698caf0eee0d21
ronniebm <ronnie.coding@gmail.com>
2020-05-12 22:21:51 -0500

    test visual

---
+++ functions2.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * _add - it adds an element.
+ * @stack: db-pointer to a stack structure.
+ * @line_number: number of the line.
+ * Return: void.
+ */
+void _add(stack_t **stack, unsigned int line_number)
+{
+  (void)stack;
+  (void)line_number;
+  printf("funtion _add\n");
+}
+
+
+/**
+ * _nop - Monty's Nop function.
+ * @stack: db-pointer to a stack structure.
+ * @line_number: number of the line.
+ * Return: void.
+ */
+void _nop(stack_t **stack, unsigned int line_number)
+{
+  (void)stack;
+  (void)line_number;
+  printf("funtion _nop\n");
+}
