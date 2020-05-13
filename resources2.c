69c5ac2f3b6be39cf55250d3d4166224850b058d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-13 09:36:49 -0500

    create file resources2.c

---
+++ resources2.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+/**
+ * read_textfile - funtion for read file
+ * @filename: name of file
+ * @letters: length of character to read in the file
+ * Return: number the character reads
+ */
+
+char *read_textfile(const char *filename, size_t letters)
+{
+	int fb, read_int;
+	/* best practique is better usar malloc that array */
+	//char *buf = (char *)malloc(sizeof(char) * letters);
+  char *buf = NULL;
+  size_t buff_size = 0;
+
+	/* if failure malloc o the string filename is null return o */
+	if (filename == NULL || buf == NULL)
+		return (0);
+
+	/* if fauilure open file return 0 */
+	fb = open(filename, O_RDONLY);
+	if (fb == -1)
+		return (0);
+	
+	/* if fauilure read file return 0 */
+  //read_int = getline(&buf, &buff_size, fb);
+	read_int = read(fb, buf, letters);
+	if (read_int == -1)
+		return (0);
+
+	/* add null caracter for indicate finish of string */
+	buf[read_int] = '\0';
+	close(fb);
+
+	return (buf);
+}
