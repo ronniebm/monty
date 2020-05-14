eedf7b8b736d166be64501c9a6ac03ff0bc1df5c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-14 14:32:21 -0500

    funtion mul work and betty done

---
+++ mul.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+* _mul - mul the second top element of the stack with the top
+* @stack: db-pointer to a stack structure.
+* @line_number: number of the line.
+* Return: void.
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+	int num;
+	stack_t *top_elemnt = NULL;
+
+	if (stack_len() >= 2)
+	{
+		top_elemnt = (*stack);
+		(*stack) = (*stack)->next;
+		num = (*stack)->n * top_elemnt->n;
+		(*stack)->n = num;
+		free(top_elemnt);
+		(*stack)->prev = NULL;
+		top_elemnt = NULL;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit_program();
+	}
+}
